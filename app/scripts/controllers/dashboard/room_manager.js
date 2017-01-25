8a21272809869b997bdc76276eafc61305746a79
rinoyrix <rinoyrix@gmail.com>
2017-01-25 14:01:11 +0530

    1st commit

---
+++ app/scripts/controllers/dashboard/room_manager.js
@@ -0,0 +1,119 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alFjrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the alFjrApp
+ */
+
+angular.module('BlueUI')
+  .controller('RoomManagerCtrl', function ($rootScope,$state,headerService,roomManagerService) {
+    var vm = this;
+    headerService.setPage();
+    vm.walletObject = {wallet:{}, reports:[]};
+
+    roomManagerService.getRoomWallet(function (result) {
+      vm.walletObject = result;
+    });
+
+  })
+
+
+  .controller('notifyctrl',function($scope){
+
+    $scope.notifydata=[
+      {user:"Waxx",desc:'Ari medichu',amount:100},
+      {user:"Rixx",desc:'Vegetables',amount:20},
+      {user:"Rosh",desc:'Water refilled',amount:30},
+      {user:"Waxx",desc:'Deposit',amount:20}
+
+    ]
+    // $scope.employees = arremployees;
+  })
+
+
+
+  .controller('ReportCtrl',function ($scope, $timeout) {
+    var vm = this;
+    vm.refreshing = false;
+    vm.refresh = function() {
+      if(!vm.refreshing){
+        vm.refreshing = true;
+
+        // do ome code to refresh data
+
+        $timeout(function () {
+          vm.refreshing = false;
+        },3000);
+
+      }
+    };
+  })
+  .controller('GraphCtrl',function ($scope, $timeout) {
+    var vm = this;
+    vm.refreshing = false;
+    vm.refresh = function() {
+      if(!vm.refreshing){
+        vm.refreshing = true;
+
+        // do ome code to refresh data
+
+        $timeout(function () {
+          vm.refreshing = false;
+        },3000);
+
+      }
+    };
+  })
+  .controller('InsertCtrl',function ($scope, $timeout) {
+    var vm = this;
+    vm.refreshing = false;
+    vm.refresh = function() {
+      if(!vm.refreshing){
+        vm.refreshing = true;
+
+        // do ome code to refresh data
+
+        $timeout(function () {
+          vm.refreshing = false;
+        },3000);
+
+      }
+    };
+  })
+  .controller('CurrentBalCtrl',function ($scope, $timeout) {
+    var vm = this;
+    vm.refreshing = false;
+    vm.refresh = function() {
+      if(!vm.refreshing){
+        vm.refreshing = true;
+
+        // do ome code to refresh data
+
+        $timeout(function () {
+          vm.refreshing = false;
+        },3000);
+
+      }
+    };
+  });
+
+
+
+
+/*
+  Different categories of notification cat
+
+  ROOM_WALLET
+
+
+  Different types of notification
+
+ FUND_INSERTED
+ FUND_RETRIEVED
+ ITEM_PURCHASED
+
+
+ */
